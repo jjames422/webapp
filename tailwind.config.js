b0fe558f93879faf292d53db10302554c784a286
Jonathan <jjames422@me.com>
2023-07-06 06:07:46 -0400

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,6 +13,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        game: ['Video Game Font', ...defaultTheme.fontFamily.sans],
+      }
     },
   },
   plugins: [],
